a4cb13b17a304b84163a63504eeab67edd29ec32
Minho Tokuhara <minho.ny@gmail.com>
2023-10-08 18:19:50 +0800

    second review

--- index.ts
+++ index.ts
@@ -1,30 +1,20 @@
-class ObjectWrapper {
-  private _obj: { [key: string]: unknown };
+// ジェネリクスのクラス Tはオブジェクト型
+// extends句により型パラメータに制約をつける
+class ObjectWrapper<T extends Record<string, any>> {
+  private _obj: T;
 
-  /***
-   * 引数のオブジェクトのコピーを this._objに設定
-   */
-  // キーは文字列、値は不特定の型
-  // コピーにより外部オブジェクトの変更から隔離、アクセスのみ加
-  constructor(_obj: { [key: string]: unknown }) {
+  // オブジェクトのコピーを保存、外部からは独立
+  constructor(_obj: T) {
     this._obj = { ..._obj };
   }
 
-  /**
-   * this._objのコピーを返却
-   * @return Object
-   */
-  get obj() {
+  get obj(): T {
     return { ...this._obj };
   }
 
-  /**
-   * this._obj[key] に valを設定。keyがthis._objに存在しない場合、falseを返却
-   * @param key オブジェクトのキー
-   * @param val オブジェクトの値
-   */
-  set(key: string, val: unknown): boolean {
-    if (this._obj.hasOwnProperty(key)) {
+  // キーと値を保存、無い場合はFalseを返す
+  set<K extends keyof T>(key: K, val: T[K]): boolean {
+    if (key in this._obj) {
       this._obj[key] = val;
       return true;
     } else {
@@ -32,21 +22,21 @@ class ObjectWrapper {
     }
   }
 
-  /**
-   * 指定したキーの値のコピーを返却
-   * 指定のキーが存在しない場合 undefinedを返却
-   * @param key オブジェクトのキー
-   */
-  get(key: string) {
-    // ?:は条件式のTrue/Falseの評価をする
-    return this._obj.hasOwnProperty(key) ? this._obj[key] : undefined;
+  // 指定されたキーに対応する値を返す、存在しない場合undefinedを返す
+  get<K extends keyof T>(key: K): T[K] | undefined {
+    return this._obj[key];
   }
 
-  /**
-   * 指定した値を持つkeyの配列を返却。該当のものがなければ空の配列を返却。
-   */
-  findKeys(val: unknown) {
-    return Object.keys(this._obj).filter((key) => this._obj[key] == val);
+  // 指定されて値に対するキーの配列を返す
+  // 一致する全てのキーを配列に格納して返す
+  findKeys(val: T[keyof T]): (keyof T)[] {
+    const result: (keyof T)[] = [];
+    for (const key in this._obj) {
+      if (this._obj[key] === val) {
+        result.push(key);
+      }
+    }
+    return result;
   }
 }
 
@@ -55,25 +45,23 @@ class ObjectWrapper {
  * 完成したら、以下のスクリプトがすべてOKになる。
  */
 const obj1 = { a: "01", b: "02" };
-const wrappedObj1 = new ObjectWrapper(obj1);
-
-if (wrappedObj1.obj.a === "01") {
-  console.log("OK: get obj()");
-} else {
-  console.error("NG: get obj()");
-}
+// const wrappedObj1 = new ObjectWrapper(obj1);
+// ジェネリクス型を明示的に指定、型の安全性を確保
+const wrappedObj1 = new ObjectWrapper<{ a: string; b: string }>(obj1);
 
 if (
-  wrappedObj1.set("c", "03") === false &&
-  wrappedObj1.set("b", "04") === true &&
-  wrappedObj1.obj.b === "04"
+  wrappedObj1.set("c" as keyof typeof obj1, "03" as string) === false &&
+  wrappedObj1.set("b" as keyof typeof obj1, "04" as string) === true
 ) {
   console.log("OK: set(key, val)");
 } else {
   console.error("NG: set(key, val)");
 }
 
-if (wrappedObj1.get("b") === "04" && wrappedObj1.get("c") === undefined) {
+if (
+  wrappedObj1.get("b" as keyof typeof obj1) === "04" &&
+  wrappedObj1.get("c" as keyof typeof obj1) === undefined
+) {
   console.log("OK: get(key)");
 } else {
   console.error("NG: get(key)");
